95058dcb64b92503b9be35439597dd215075e734
SkateYoung <Dimm0L@163.com>
2024-06-01 14:11:53 +0800

    Site updated: 2024-06-01 14:11:24

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// 复制提醒
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 1000
+                });
+            }
+        })
+    }, 300);
+})
 document.addEventListener('DOMContentLoaded', function () {
   let headerContentWidth, $nav
   let mobileSidebarOpen = false
@@ -90,19 +116,6 @@ document.addEventListener('DOMContentLoaded', function () {
       if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
         document.execCommand('copy')
         alertInfo(ctx, GLOBAL_CONFIG.copy.success)
-        new Vue({
-        data: function () {
-        this.$notify({
-            title: "你已被发现😜",
-            message: "小伙子，扒源记住要遵循GPL协议！",
-            position: 'top-left',
-            offset: 50,
-            showClose: true,
-            type: "warning",
-            duration: 5000
-        });
-    }
-})
       } else {
         alertInfo(ctx, GLOBAL_CONFIG.copy.noSupport)
       }
